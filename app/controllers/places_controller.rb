e1fd684b360202d2fa6e379483738aec80c01d10
Tracy Li <tracy.li@kellogg.northwestern.edu>
2024-03-09 22:29:30 +0000

    changed

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,17 +6,28 @@ class PlacesController < ApplicationController
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] })
+    @user = User.find_by({ "id" => session["user_id"] })
+        if @user != nil
+      @entries = Entry.where({"place_id" => @place["id"],"user_id" => @user["id"]})
+    else
+      flash["notice"] = "Login first."
+      redirect_to "/login"
+    end
   end
 
   def new
   end
 
   def create
-    @place = Place.new
-    @place["name"] = params["name"]
-    @place.save
-    redirect_to "/places"
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user != nil
+      @place = Place.new
+      @place["name"] = params["name"]
+      @place.save
+    else
+      flash["notice"] = "Login first."
+    end
+      redirect_to "/places"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources "places"
   resources "sessions"
   resources "users"
+  # Define a route for the login page
+get "/login", :controller => "sessions", :action => "new"
+get "/logout", :controller => "sessions", :action => "destroy"
 end
